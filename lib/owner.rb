2d4b720fed5d70468e7739dee7151af4d384e369
Cody Toney-Griffith <cody.toney@gmail.com>
2020-04-29 21:21:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -9,37 +9,37 @@ class Owner
     @@all<<self
    @pets = {:dogs => [], :cats => []}
   end
-  
+
   def say_species
 "I am a #{species}."
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.count
     @@all.length
   end
-  
+
   def self.reset_all
     @@all.clear
   end
-  
+
   def pets
     @pets = pets
   @dogs = dog
   @cats = cat
   end
-  
+
   def cats
 Cat.all.select {|cat| cat.owner == self }
   end
-  
+
   def buy_dog(dog)
     @pets[:dogs] << Dog.new(dog)
   end
-  
+
   def walk_dogs
   @pets[:dogs].each do |dog|
     dog.mood = "happy"
@@ -55,7 +55,7 @@ end
   def buy_cat(cat)
     @pets[:cats]<< Cat.new(cat)
   end
-  
+
   def list_pets
    "I have #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
  end
